835b15820988468e9f7a95371f164ae9506b25a6
Sarthak1503 <www.sarthak1503@gmail.com>
2018-06-18 15:57:40 +0530

    1st assignment is submitted

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+#GIT COMMANDS
+git config --global user.name"USERNAME"
+git config --global user.email"EMAIL ID"
+git init #for initialisation
+git status #for checking the changes before & after execution
+git add filename #to add files (one at a time)
+git add . #to add all files at a time
+git commit -m"YOUR MESSAGE" #for personal preference where the changes made
+git log #show all the commits we made
+git remote add origin #to copy the link from repository
+git push origin master #to upload the file to github
+
+#git updations
+git add filename
+git status
+git commit -m"YOUR MESSAGE"
+git push origin master
