16efc37ecde0c573dbf856b04895862a533ebfc3
Sarthak1503 <www.sarthak1503@gmail.com>
2018-06-20 13:03:21 +0530

    2nd assignment added

---
+++ Assignment2.py
@@ -0,0 +1,31 @@
+#1.PRINT ANYTHING ON SCREEN
+print("ASSIGNMENT NUMBER 2")
+
+
+#2.JOIN TWO STRINGS USING '+'
+S1="GURDEEP"
+S2="SINGH"
+print(S1+S2)
+
+
+#3.TAKE THE I/P OF 3 VARIABLES X,Y & Z.PRINT THEIR VALUES ON SCREEN
+x=int(input("ENTER THE VALUE OF X="))
+Y=int(input("ENTER THE VALUE OF Y="))
+Z=int(input("ENTER THE VALUE OF Z="))
+print(x,Y,Z)
+
+#4.PRINT "LET'S GET STARTED" ON SCREEN
+print("LET'S GET STARTED")
+
+
+#5.PRINT THE FOLLOWING VALUES USING PLACEHOLDERS.
+s="Acadview"
+course="Python"
+fees=5000
+print("s:%s\nCourse:%s\nfees:%d"%(s,course,fees))
+
+
+#6.LET'S DO SOMETHING INTERESTING EXERCISE:
+name="Tony Stark"
+salary=1000000
+print("name:%s\nsalary:%d\n"%(name,salary))
